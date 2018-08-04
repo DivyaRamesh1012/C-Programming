fec3e00dde5ab1e846b99deccf35d200539fb8ed
DivyaRamesh1012 <38650961+DivyaRamesh1012@users.noreply.github.com>
2018-08-04 06:41:22 +0000

    Update armstrong.c

--- Code Kata/armstrong.c
+++ Code Kata/armstrong.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
-#include <math.h>
-void main()
+#include<stdio.h>
+int main()
 {
-    int n,sum=0,rem=0,x=0,t;
+    int n,a,rem,t=0;
     scanf("%d",&n);
-    t=n;
-    while(n!=0)
+    a=n;
+    while(a!=0)
     {
-        rem=n%10;
-        x=pow(rem,3);
-        sum=sum+x;
-        n=n/10;
+        rem=a%10;
+        t+=(rem*rem*rem);
+        a/=10;
     }
-    if(sum==t)
-        printf ("yes");
+    if(t==n)
+        printf("yes");
     else
-        printf ("no");
+        printf("no");
+    return 0;
 }
+    
+
