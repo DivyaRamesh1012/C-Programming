801b8182d23fb243aad4ed824f644c431d198ef9
DivyaRamesh1012 <38650961+DivyaRamesh1012@users.noreply.github.com>
2018-08-28 11:05:40 +0000

    Update odd range.c

--- Code Kata/odd range.c
+++ Code Kata/odd range.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int a,b,q=100000,i;
-scanf("%d%d",&a,&b);
-if(b<q)
-{
-for(i=a+1;i<=b;i++)
-{
-if(i%2!=0)
-{
-printf("%d\t",i);
-}
-}
-}
+  int a,b,q=100000,i;
+  scanf("%d%d",&a,&b);
+  if(b<q)
+  {
+    for(i=a+1;i<=b;i++)
+    {
+      if(i%2!=0)
+      {
+        printf("%d\t",i);
+      }
+    }
+  }
 }
